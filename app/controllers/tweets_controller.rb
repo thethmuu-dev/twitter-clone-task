151fd0524616c196dca1475bc583691e7f44d8af
thethmuu-dev <thethmuueainsoe@gmail.com>
2020-08-04 11:58:34 +0630

    Index Page of All Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,14 @@
 class TweetsController < ApplicationController
+  def index
+  @tweets = Tweet.all
+  end
+
   def new
   @tweet = Tweet.new
   end
 
   def create
   Tweet.create(content: params[:tweet][:content])
+  redirect_to tweets_path
   end
 end
