00433c5f3486598bd05a89238eed7413344d250b
thethmuu-dev <richardfromsv@gmail.com>
2020-08-04 14:02:54 +0630

    Tweet Show and Delete Functions

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :destroy]
   def index
   @tweets = Tweet.all
   end
@@ -8,7 +9,28 @@ class TweetsController < ApplicationController
   end
 
   def create
-  Tweet.create(content: params[:tweet][:content])
-  redirect_to tweets_path
+  @tweet = Tweet.new(tweet_params)
+  if @tweet.save
+  redirect_to tweets_path, notice: "Tweet Posted!"
+  else
+    render :new
+  end
+  end
+
+  def show
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweet Deleted"
+  end
+
+  private
+  def tweet_params
+  params.require(:tweet).permit(:content)
+  end
+
+  def set_tweet
+  @tweet = Tweet.find(params[:id])
   end
 end
